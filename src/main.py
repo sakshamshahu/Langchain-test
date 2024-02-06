6304b5fd7ea15b70a16e7f92bb60d51f17f94a82
sakshamshahu <sakshamshahu.work@gmail.com>
2024-02-06 18:11:39 +0530

    feat: unstructured pdf loader
    Signed-off-by: sakshamshahu <sakshamshahu.work@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import UnstructuredPDFLoader
 
-loader = PyPDFLoader("/Users/saksham/Projects/Langchain-test/data/raw/sample_data/30_pages_struct.pdf")
-pages = loader.load_and_split()
+loader = UnstructuredPDFLoader("/Users/saksham/Projects/Langchain-test/data/raw/sample_data/30_pages_struct.pdf")
+data = loader.load()
+print(data)
 
 load = PyPDFLoader('/Users/saksham/Projects/Langchain-test/data/raw/sample_data/new-solution-to-the-midas-touch-problem-identification-of-visual-commands-via-extraction-of-focal-fixations.pdf')
 page = load.load_and_split()
 
-print(pages)
+print(page)

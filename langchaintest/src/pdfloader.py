946df54b1e94e9096eea900f95719907d394daad
sakshamshahu <sakshamshahu.work@gmail.com>
2024-02-20 14:37:51 +0530

    refactor: moved code to different file
    Signed-off-by: sakshamshahu <sakshamshahu.work@gmail.com>

---
+++ langchaintest/src/pdfloader.py
@@ -0,0 +1,11 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import UnstructuredPDFLoader
+
+loader = UnstructuredPDFLoader("/Users/saksham/Projects/Langchain-test/langchaintest/data/raw/sample_data/30_pages_struct.pdf" , mode="elements" )
+data = loader.load()
+print(data)
+
+# load = PyPDFLoader('/Users/saksham/Projects/Langchain-test/data/raw/sample_data/new-solution-to-the-midas-touch-problem-identification-of-visual-commands-via-extraction-of-focal-fixations.pdf')
+# page = load.load_and_split()
+
+# print(page)

3b741f06110e75671bdafc4420e195f1e031226c
sakshamshahu <sakshamshahu.work@gmail.com>
2024-02-21 06:57:19 +0530

    feat: chroma implementation
    Signed-off-by: sakshamshahu <sakshamshahu.work@gmail.com>

---
+++ langchaintest/src/chroma.py
@@ -0,0 +1,19 @@
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.document_loaders import TextLoader
+from langchain_community.embeddings.sentence_transformer import (SentenceTransformerEmbeddings,)
+from langchain_community.vectorstores import Chroma
+
+loader = TextLoader("langchaintest/data/raw/sample_data/structured.txt")
+documents = loader.load()
+
+text_splitter = CharacterTextSplitter(chunk_size=50, chunk_overlap=0, separators=[" ", ",", "\n"]) # check func of this
+docs = text_splitter.split_documents(documents)
+
+embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+
+db = Chroma.from_documents(docs, embedding_function)
+
+query = "What is ventral stream"
+docs = db.similarity_search(query)
+
+print(docs[0].page_content)

fd268b52f1c4af65cfe4b2f9f21f6ee001e221be
sakshamshahu <sakshamshahu.work@gmail.com>
2024-02-21 07:00:08 +0530

    feat: change in chunk separators
    Signed-off-by: sakshamshahu <sakshamshahu.work@gmail.com>

--- langchaintest/src/chroma.py
+++ langchaintest/src/chroma.py
@@ -1,4 +1,5 @@
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
 from langchain_community.embeddings.sentence_transformer import (SentenceTransformerEmbeddings,)
 from langchain_community.vectorstores import Chroma
@@ -6,7 +7,10 @@ from langchain_community.vectorstores import Chroma
 loader = TextLoader("langchaintest/data/raw/sample_data/structured.txt")
 documents = loader.load()
 
-text_splitter = CharacterTextSplitter(chunk_size=50, chunk_overlap=0, separators=[" ", ",", "\n"]) # check func of this
+text_splitter = RecursiveCharacterTextSplitter(
+chunk_size=1000, chunk_overlap=0, separators=[",", "\n"]
+)
+# text_splitter = CharacterTextSplitter(chunk_size=50, chunk_overlap=0, separators=[" ", ",", "\n"]) # check func of this
 docs = text_splitter.split_documents(documents)
 
 embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
